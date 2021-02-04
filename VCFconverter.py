31961617a0922b091612451bd6d7512f37966cc0
biezz0210 <biezz0210@gmail.com>
2021-02-04 14:26:27 +0900

    add progress bar

--- VCFconverter.py
+++ VCFconverter.py
@@ -21,20 +21,40 @@ class MainDialog(QDialog):
         self.convert.clicked.connect(self.button_convert)
 
 
+
     def button_click_open(self):
         file_name = QFileDialog.getOpenFileName(self)
         self.textEdit.setText(file_name[0])
         _file.append(file_name[0])
+
+        self.progress_label.setText('selected')
+        self.progressBar.setValue(0)    
 
 
     def button_convert(self):
 
         ver = self.comboBox.currentText()
 
-        df = pd.read_csv('%s'%_file[-1], compression='gzip', sep='\t',header=None ,error_bad_lines=False,warn_bad_lines=False)
-        x,y = df.shape
-        df = pd.read_csv('%s'%_file[-1], compression='gzip', sep='\t',skiprows=x)
+        self.progressBar.setRange(0,0)
+        self.progress_label.setText('ongoing')
+
+        try:
+            self.progressConvert(_file[-1], ver)
+            self.progressBar.setRange(0,100)
+            self.progressBar.setValue(100)
+            txt = emoji.emojize('Done :thumbs_up:')
+            self.progress_label.setText('%s'%txt)
+
+        except:
+            self.progress_label.setText('Error: This file is not supported')
+
+
+    def progressConvert(self, fileName, ver):
         
+        df = pd.read_csv('%s'%fileName, compression='gzip', sep='\t',header=None ,error_bad_lines=False,warn_bad_lines=False)
+        x,y = df.shape
+        df = pd.read_csv('%s'%fileName, compression='gzip', sep='\t',skiprows=x)
+
         m = [x.split(';')[0] != 'INDEL' for x in df['INFO']]
         df_snp = df[m]
         
@@ -46,6 +66,7 @@ class MainDialog(QDialog):
 
         df_snp['ix'] = list(map(sum_data, chrom, pos))
 
+
         if ver[-3:] == '1.0':
             df_db = pd.read_csv('./data/Ver1DB.csv')
         else:
@@ -54,6 +75,7 @@ class MainDialog(QDialog):
         df_snp_ix = df_snp.set_index('ix')
         df_db_ix = df_db.set_index('POSpre')
         df_db_common = df_db_ix.loc[common_ix]
+
 
         chr_fix = list(map(lambda x : x.split('-')[0], df_db_common['POSv4']))
         pos_fix = list(map(lambda x : int(x.split('-')[1]), df_db_common['POSv4']))
@@ -76,19 +98,23 @@ class MainDialog(QDialog):
         df['sort'] = list(map(sorted_chr, df['#CHROM']))
         df_sort = df.sort_values(by=['sort', 'POS'])
 
-        header = '''##reference=glyma.Wm82.gnm4.4PTR.genome_main.fna\n'''
+        df_sort = df_sort[df_sort.columns[:-1]]
 
-        txt = emoji.emojize('Done :thumbs_up:')
-        self.label_result.setText('%s'%txt)
+        header = '''##reference=glyma.Wm82.gnm4.4PTR.genome_main.fna\n'''
 
 
         name = QFileDialog.getSaveFileName(self, 'Open file', './')
-        name = name[0] + '.vcf'
+        if name[0] == '':
+            name = './sample.vcf'
+        else:
+            name = name[0] + '.vcf'
 
         with open(name, 'w') as vcf:
             vcf.write(header)
 
         df_sort.to_csv(name, sep='\t', mode='a', index=False)
+
+
 
 
 if __name__ == '__main__':
